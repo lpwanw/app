1be81900a48d0abf7bbad01a75484d35701fccaa
Lê Phương Tây <41092293+lpwanw@users.noreply.github.com>
2021-06-30 13:53:32 +0700

    update

---
+++ src/component/TodoItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./TodoItem.css";
+class TodoItem extends Component {
+  render() {
+    //
+    const { item } = this.props;
+    let className = "TodoItem";
+    if (item.isCompleted) {
+      className += " TodoItem-complete";
+    }
+    return (
+      <div className={className}>
+        <p>{this.props.item.title}</p>
+      </div>
+    );
+  }
+}
+export default TodoItem;
